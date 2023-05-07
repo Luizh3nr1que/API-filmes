29f8957a70c4dac7a97b167ff0420c1c55e7a7a5
luizh3nrique <luizh3nrique00@gmail.com>
2023-05-06 23:17:49 -0300

    Signed-off-by: luizh3nrique <luizh3nrique00@gmail.com>

--- script.js
+++ script.js
@@ -2,24 +2,30 @@ const API_KEY = '6a5150a19a26986f9fe81b52ff4eeda3';
 const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const language = 'language=pt-BR';
+const erro = document.getElementById('erro');
 
 function getMovie() {
-    const movieId = Math.floor(Math.random() * 2000) + 1;
+    const movieId = Math.floor(Math.random() * 1000) + 1;
     const url = `${BASE_URL}${movieId}?api_key=${API_KEY}&${language}`;
 
     fetch(url)
-        .then(response => response.json())
+        .then(response => {
+            if (response.status === 404) {
+                erro.innerHTML = 'Lamentamos, mas a API não conseguiu localizar o filme';
+            } else {
+                erro.innerHTML = ''
+            }
+            return response.json();
+        })
         .then(data => {
-
             title.textContent = data.title;
             resumo.textContent = data.overview;
-
             capa.src = `${IMG_URL}${data.poster_path}`;
             document.body.appendChild(img);
-
         })
         .catch(error => console.error(error));
 };
+
 
 
 
@@ -30,6 +36,11 @@ window.addEventListener('blur', () => {
 window.addEventListener('focus', () => {
     document.title = docTitle;
 });
+
+
+
+
+
 
 
 
